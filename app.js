df6e3882ab933b774b7d23b4bf48266e86392d44
R praveen kumar <praveen3554@gmail.com>
2024-04-18 19:20:28 +0530

    nodepro

---
+++ app.js
@@ -0,0 +1,11 @@
+const express=require('express');
+
+const app = express();
+
+app.listen(8080,()=>{
+    console.log("server is listening on port 8080");
+});
+
+app.get("/home",(req, res)=>{
+    res.send('<h1>welcome to the home page<h1/>');
+})
